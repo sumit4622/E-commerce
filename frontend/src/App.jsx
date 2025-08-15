84d384fb4f9f09ae8ae18077944e83b27d0a0355
sumitraay2022@gmail.com <sumitraay2022@gmail.com>
2025-08-15 21:48:16 +0545

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import Login from './Pages/Login'
+import Header from './component/Header'
+import Register from './Pages/Register'
+
+import { Routes, Route } from 'react-router-dom';
+
+    // import './App.css'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path='/' element={<Header />}/>
+    </Routes>
+  )
+}
+
+export default App
